6ebb5ec647415436531d925d4ccf7379ebef71cd
Francesco Loreti <francesco.loreti@outlook.com>
2020-06-12 01:20:49 +0200

    Update loader.js
    Eliminate duplicazioni

--- app/javascript/packs/loader.js
+++ app/javascript/packs/loader.js
@@ -1,13 +1,16 @@
-document.addEventListener('ajax:beforeSend', function(event) {
-  event.target.classList.add('is-loading')
-  document.getElementById('loader').classList.remove('is-hidden')
-  })
-document.addEventListener('ajax:success', function(event) {
-  event.target.classList.remove('is-loading')
-  document.getElementById('loader').classList.add('is-hidden')
-  })
+const loading = (event) => { event.target.classList.add('is-loading'); document.getElementById('loader').classList.remove('is-hidden')};
+const loaded = (event) => {  event.target.classList.remove('is-loading'); document.getElementById('loader').classList.add('is-hidden') };
+const activate = (target) => { document.getElementById(target).classList.add('is-active') };
+
+document.addEventListener('ajax:beforeSend', function(event) { loading(event) })
+document.addEventListener('ajax:success', function(event) { loaded(event) })
 document.addEventListener('ajax:error', function(event) {
-  document.getElementById('error').classList.add('is-active')
-  event.target.classList.remove('is-loading')
-  document.getElementById('loader').classList.add('is-hidden')
+   console.log(event);
+   if (event.detail[1] == "Unauthorized") {
+     loaded(event);
+     activate('timeout');
+   } else {
+     loaded(event);
+     activate('error') ;
+   }
  })
