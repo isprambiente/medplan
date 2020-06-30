da83565f4cfe88640b47a194c482ebb1a6571a52
francesco.loreti <francesco.loreti@isprambiente.it>
2020-06-30 19:37:40 +0200

    Rimosso parametro secret.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'medplan'
 set :repo_url, 'https://github.com/isprambiente/medplan'
 set :deploy_to, '/home/medplan'
 # set :pty, true
-set :linked_files, fetch(:linked_files, []).push('config/master.key', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/sessions', 'tmp/state', 'vendor/bundle', 'public/system', 'config/settings', 'storage')
 
 set :tmp_dir, '/home/medplan/shared/tmp'
