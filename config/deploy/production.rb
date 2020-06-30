e1643cad535679fcc2c59795d0f5b59298412145
francesco.loreti <francesco.loreti@isprambiente.it>
2020-06-30 20:39:43 +0200

    Modificata configurazione per deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-server Settings.deploy.host, user: 'medplan', roles: %w[web app db]
+server 'test.yourserver.com', user: 'medplan', roles: %w[web app db]
 set :deploy_to, '/home/medplan/production'
 set :rails_env, 'production'
 set :branch, 'master'
