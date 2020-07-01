531ac6d7fec03b15f7033896f981bf8dd9add924
francesco.loreti <francesco.loreti@isprambiente.it>
2020-07-01 13:29:27 +0200

    Modificata procedura deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'medplan'
 set :repo_url, 'https://github.com/isprambiente/medplan'
 set :deploy_to, '/home/medplan'
 # set :pty, true
-set :linked_files, fetch(:linked_files, []).push('config/master.key', 'config/credentials.yml.enc', 'config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/master.key', 'config/credentials.yml.enc')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/sessions', 'tmp/state', 'vendor/bundle', 'public/system', 'config/settings', 'storage')
 
 set :tmp_dir, '/home/medplan/shared/tmp'
@@ -15,3 +15,14 @@ set :keep_releases, 5
 # after 'deploy:finishing',     'deploy:cleanup'
 before 'deploy:log_revision', 'puma:stop'
 before 'deploy:log_revision', 'puma:start'
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        upload! 'config/master.key', "#{shared_path}/config/master.key"
+        upload! 'config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc"
+      end
+    end
+  end
+end
