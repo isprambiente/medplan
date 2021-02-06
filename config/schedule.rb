ef2c360c70104fa04c98a32106a017eb54cb9b40
francesco.loreti <francesco.loreti@isprambiente.it>
2021-02-06 14:15:37 +0100

    Aggiornamento librerie

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+ENV.each { |k, v| env(k, v) }
+set :output, "/home/medplan/#{ENV['RAILS_ENV']}/current/log/cron_log_#{ENV['RAILS_ENV']}.log"
+
 every 3.hours, roles: [:app] do
   runner 'UsersChecknewJob.perform_now'
 end
