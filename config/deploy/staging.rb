bf78384c7cbae899335c215f494ae1fc2a3a3de4
francesco.loreti <francesco.loreti@isprambiente.it>
2021-03-24 10:53:59 +0100

    Upgrade to Ruby 3.0

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ server 'test.yourserver.com', user: 'medplan', roles: %w[web app db]
 set :deploy_to, '/home/medplan/staging'
 set :rails_env, 'staging'
 set :branch, 'staging'
-set :rvm_ruby_version, '2.7.2@medplan'
+set :rvm_ruby_version, '3.0.0@medplan'
 
 namespace :deploy do
   namespace :check do
