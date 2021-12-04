db6b6a947adeba1b9e4166734d37bbda14dec17a
francesco.loreti <francesco.loreti@isprambiente.it>
2021-12-04 19:13:32 +0100

    Aggiunta autorizzazione hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts = nil
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
