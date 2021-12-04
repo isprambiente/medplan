2aa73042125432dfad0b963b3120a938d62b5f5f
francesco.loreti <francesco.loreti@isprambiente.it>
2021-12-04 15:06:19 +0100

    Passaggio a ruby 3.0.3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ server 'medplan.intranet.isprambiente.it', user: 'medplan', roles: %w[web app db
 set :deploy_to, '/home/medplan/production'
 set :rails_env, 'production'
 set :branch, 'master'
-set :rvm_ruby_version, '3.0.1@medplan'
+set :rvm_ruby_version, '3.0.3@medplan'
 
 namespace :deploy do
   namespace :check do
