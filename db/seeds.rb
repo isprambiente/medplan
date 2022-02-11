d81248053a9368ba0bce7541eae8e2501a011c98
francesco.loreti <francesco.loreti@isprambiente.it>
2022-02-11 13:27:25 +0100

    Remove uncorrect parameters after upgrade to devise_cas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
-dottore = User.new(username: 'dottore', label: 'Dottore', name: 'Dottore', password: 'dottore', password_confirmation: 'dottore', city: :roma, doctor: true, cf: 'DOTTORE1', system: true)
+admin = User.new(username: 'admin', label: 'Administrator', name: 'Administrator', city: :other, cf: 'Admin1', admin: true, system: true)
+admin.save(validate: false)
+admin.reset_password('admin','admin')
+dottore = User.new(username: 'dottore', label: 'Dottore', name: 'Dottore', city: :roma, doctor: true, cf: 'DOTTORE1', system: true)
 dottore.save(validate: false)
-segretaria = User.new(username: 'segretaria', label: 'Segretaria', name: 'Segretaria', password: 'segretaria', password_confirmation: 'segretaria', city: :roma, secretary: true, cf: 'SEGRETARIA1', system: true)
+dottore.reset_password('dottore','dottore')
+segretaria = User.new(username: 'segretaria', label: 'Segretaria', name: 'Segretaria', city: :roma, secretary: true, cf: 'SEGRETARIA1', system: true)
 segretaria.save(validate: false)
-mario = User.new(username: 'mario.rossi', label: 'Rossi Mario', name: 'Mario', lastname: 'Rossi', password: 'mario', password_confirmation: 'mario', city: :roma, cf: 'RSSMRA69A01H501Z', system: true)
+segretaria.reset_password('segretaria','segretaria')
+mario = User.new(username: 'mario.rossi', label: 'Rossi Mario', name: 'Mario', lastname: 'Rossi', city: :roma, cf: 'RSSMRA69A01H501Z', system: true)
 mario.save(validate: false)
-utente = User.new(username: 'giorgio.verdi', label: 'Verdi Giorgio', name: 'Giorgio', lastname: 'Verdi', password: 'giorgio', password_confirmation: 'giorgio', city: :venezia, cf: 'VRDGRG80A01L736Q')
+mario.reset_password('mario','mario')
+utente = User.new(username: 'giorgio.verdi', label: 'Verdi Giorgio', name: 'Giorgio', lastname: 'Verdi', city: :venezia, cf: 'VRDGRG80A01L736Q')
 utente.save(validate: false)
-utente = User.new(username: 'grazia.bianchi', label: 'Bianchi Grazia', name: 'Grazia', lastname: 'Bianchi', password: 'grazia', password_confirmation: 'grazia', cf: 'BNCGRZ63A41F205Z', city: :livorno)
+utente.reset_password('giorgio','giorgio')
+utente = User.new(username: 'grazia.bianchi', label: 'Bianchi Grazia', name: 'Grazia', lastname: 'Bianchi', cf: 'BNCGRZ63A41F205Z', city: :livorno)
 utente.save(validate: false)
+utente.reset_password('grazia','grazia')
+50.times do |n|
+  u = User.create(username: "user#{n}", label: "User ##{n}", name: "User ##{n}", cf: "user#{n}", city: :other  ); u.save(validate: false)
+end
 
 Category.create(code: 'cata', title: 'categoria a', months: 6)
 Category.create(code: 'catb', title: 'categoria b', months: 12)
