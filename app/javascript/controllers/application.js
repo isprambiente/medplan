1aa26ab4b938aece8797856874dc458cd6f27e46
francesco.loreti <francesco.loreti@isprambiente.it>
2022-04-20 15:33:15 +0200

    Rimosso debug

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -12,8 +12,8 @@ document.querySelectorAll('[disabled]').forEach(function(obj) {
 window.addEventListener("click",function(t){if("click->menu#open"!=t.target.getAttribute("data-action")){const e=document.querySelector("#cart_col.quickview"),c=document.querySelector("#filters_col.quickview"),mm=document.querySelector("#menu_col.quickview");null==mm||mm.contains(t.target)||mm.classList.remove("is-active"),null==e||e.contains(t.target)||e.classList.remove("is-active"),null==c||c.contains(t.target)||c.classList.remove("is-active")}t.stopPropagation()});
 
 // Configure Stimulus development experience
-application.warning = true
-application.debug = true
+application.warning = false
+application.debug = false
 window.Stimulus   = application
 
 export { application }
