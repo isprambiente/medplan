a377372bb38728d2f68164d1d21ee505d352d906
francesco.loreti <francesco.loreti@isprambiente.it>
2022-09-16 13:41:09 +0200

    Rimosse istruzioni dopo correzione libreria

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,17 +3,8 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-import { AttachmentUpload } from "@rails/actiontext/app/javascript/actiontext/attachment_upload"
 
 import controllers from "./**/*_controller.js"
 controllers.forEach((controller) => {
   application.register(controller.name, controller.module.default)
-})
-
-addEventListener("trix-attachment-add", event => {
-  const { attachment, target } = event
-  if (attachment.file) {
-    const upload = new AttachmentUpload(attachment, target)
-    upload.start()
-  }
 })
