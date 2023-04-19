e915a2f5d2837ba8d22564aa057b106ec67f52f8
Francesco Loreti <francesco.loreti@isprambiente.it>
2023-04-19 17:17:01 +0200

    Disattivazione cron per cancellazione meetings e aggiunta data per separare i log file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 ENV.each { |k, v| env(k, v) }
-set :output, "/home/medplan/#{ENV['RAILS_ENV']}/current/log/cron_log_#{ENV['RAILS_ENV']}.log"
+set :output, "/home/medplan/#{ENV['RAILS_ENV']}/current/log/cron_log_#{ENV['RAILS_ENV']}_#{I18n.l(Time.zone.now, format: :file)}.log"
 
 every 3.hours, roles: [:app] do
   runner 'UsersChecknewJob.perform_now'
 end
 
-every 1.day, at: '0:01 am', roles: [:app] do
-  runner 'AutoDeleteProposedMeetingsJob.perform_now'
-end
+# every 1.day, at: '0:01 am', roles: [:app] do
+#   runner 'AutoDeleteProposedMeetingsJob.perform_now'
+#end
