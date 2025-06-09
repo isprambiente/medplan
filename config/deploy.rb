a616ff582bbb48002b06c19ffb072af1eccf71c9
Francesco Loreti <francesco.loreti@outlook.com>
2025-06-09 11:46:48 +0200

    Aggiunto set variabili di sistema

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,12 +11,22 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :tmp_dir, '/home/medplan/shared/tmp'
 
+
+set :RAILS_OIDC_ISSUER, ENV.fetch("RAILS_OIDC_ISSUER") { "https://my_issuer.com" }
+set :RAILS_OIDC_USERNAME, ENV.fetch("RAILS_OIDC_USERNAME") { "uid" }
+set :RAILS_PORT, ENV.fetch("RAILS_PORT") { "443" }.to_i
+set :RAILS_SCHEME, ENV.fetch("RAILS_SCHEME") { "https" }
+set :RAILS_HOST, ENV.fetch("RAILS_HOST") { "localhost" }
+set :RAILS_OIDC_IDENTIFIER, ENV.fetch("RAILS_OIDC_IDENTIFIER") { "medplan" }
+set :RAILS_OIDC_SECRET, ENV.fetch("RAILS_OIDC_SECRET") { "secret" }
+set :RAILS_SCHEME, "#{ENV.fetch("RAILS_SCHEME") { "https" }}://#{ENV.fetch("RAILS_HOST") { "localhost" }}#{ENV.fetch("RAILS_PORT") {""}}/users/auth/openid_connect/callback"
+
 set :keep_releases, 5
 
-before "deploy:assets:precompile", "deploy:yarn_install"
+before "deploy:assets:precompile", "deploy:bun_install"
 namespace :deploy do
   desc "Run rake yarn install"
-  task :yarn_install do
+  task :bun_install do
     on roles(:web) do
       within release_path do
         # execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional --production")
@@ -35,6 +45,7 @@ namespace :deploy do
       on roles(:app), in: :sequence, wait: 10 do
         upload! 'config/master.key', "#{shared_path}/config/master.key"
         upload! 'config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc"
+        upload! '.env.production.sample', ".env"
       end
     end
   end
