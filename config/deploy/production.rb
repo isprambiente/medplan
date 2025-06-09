feff9ae9ea05fafe2b64e819d1ddecb56052ece9
Francesco Loreti <francesco.loreti@outlook.com>
2025-06-09 12:09:33 +0200

    Aggiunto .env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,17 +5,6 @@ set :deploy_to, '/home/medplan/production'
 set :rails_env, 'production'
 set :branch, 'master'
 
-set :default_env, {
-  'RAILS_OIDC_ISSUER':      ENV.fetch("RAILS_OIDC_ISSUER") { "https://my_issuer.com" },
-  'RAILS_OIDC_USERNAME':    ENV.fetch("RAILS_OIDC_USERNAME") { "uid" },
-  'RAILS_PORT':             ENV.fetch("RAILS_PORT") { "443" }.to_i,
-  'RAILS_PROTOCOL':           ENV.fetch("RAILS_PROTOCOL") { "https" },
-  'RAILS_HOST':             ENV.fetch("RAILS_HOST") { "localhost" },
-  'RAILS_OIDC_IDENTIFIER':  ENV.fetch("RAILS_OIDC_IDENTIFIER") { "medplan" },
-  'RAILS_OIDC_SECRET':      ENV.fetch("RAILS_OIDC_SECRET") { "secret" },
-  'RAILS_SCHEME':           "#{ENV.fetch("RAILS_PROTOCOL") { "https" }}://#{ENV.fetch("RAILS_HOST") { "localhost" }}#{ENV.fetch("RAILS_PORT") {""}}/users/auth/openid_connect/callback"
-}
-
 namespace :deploy do
   namespace :check do
     before :linked_files, :set_master_key do
