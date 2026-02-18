8d53dbbb4ae34dd951e885adf8d8c97616f0b87b
Francesco Loreti <francesco.loreti@isprambiente.it>
2026-02-18 08:11:35 +0100

    Corretta configurazione

--- config/ci.rb
+++ config/ci.rb
@@ -5,7 +5,7 @@ CI.run do
 
   step "Style: Ruby", "bin/rubocop"
 
-  step "Security: Importmap vulnerability audit", "bin/importmap audit"
+  step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
   step "Tests: Rails", "bin/rails test"
   step "Tests: System", "bin/rails test:system"
